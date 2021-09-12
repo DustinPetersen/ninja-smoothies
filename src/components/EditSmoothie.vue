beb6f73fc1e04cfe48d1be3b01b66f122bc95b8f
dustinpetersen <dustinpetersen264@gmail.com>
2021-09-12 08:32:29 -0600

    finished project

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -25,6 +25,7 @@
 
 <script>
 import db from '@/firebase/init'
+import slugify from 'slugify'
 
 export default {
     name: 'EditSmoothie',
@@ -38,7 +39,26 @@ export default {
     },
     methods: {
         EditSmoothie(){
-            console.log(this.smoothie.title, this.smoothie.ingredients)
+            if (this.smoothie.title) {
+              this.feedback = null
+              //Create slug
+              this.smoothie.slug = slugify(this.smoothie.title, {
+                  replacement: '-',
+                  remove: /[$*_+~.()'"!\-@']/g,
+                  lower: true
+              })
+               db.collection('smoothies').doc(this.smoothie.id).update({
+                   title: this.smoothie.title,
+                   ingredients: this.smoothie.ingredients,
+                   slug: this.smoothie.slug
+               }).then(() => {
+                   this.$router.push( { name: 'index' })
+               }).catch(err => {
+                   console.log(err)
+               })
+          } else {
+              this.feedback = "Please enter a smoothie title"
+          }
         },
         addIng(){
             if(this.another){
